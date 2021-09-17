1030c70ed1cfcde6522e5bda2aedfb4f9d8a8e52
JulzPerez <julietoperez@gmail.com>
2021-09-17 00:40:46 -0700

    login changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,14 +7,15 @@
     <div class="row">
           <div class="col-md-4 login-sec">
             <!-- <h2 class="text-center">Login Now</h2> -->
-            <form class="form" method="POST" action="{{ route('login') }}">
-              @csrf
+            
 
               <div class="card card-login mb-3">
                 <div class="card-header card-header-primary text-center">
                   <h4 class="card-title"><strong>{{ __('Login') }}</strong></h4>                
                 </div>
                 <div class="card-body">
+              <form class="form" method="POST" action="{{ route('login') }}">
+              @csrf
                   <div class="bmd-form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
                     <div class="input-group">
                       <div class="input-group-prepend">
@@ -45,31 +46,43 @@
                       </div>
                     @endif
                   </div>
-                  <div class="form-check mr-auto ml-3 mt-3">
+                  <!-- <div class="form-check mr-auto ml-3 mt-3">
                     <label class="form-check-label">
                       <input class="form-check-input" type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember me') }}
                       <span class="form-check-sign">
                         <span class="check"></span>
                       </span>
                     </label>
+                  </div> -->
+                  <br>
+                  <div class="form-row mt-3">
+                    <div class="col">
+                      @if (Route::has('password.request'))
+                          <a href="{{ route('password.request') }}">
+                              {{ __('Forgot password?') }}
+                          </a>
+                      @endif
+                    </div>
+                    <div class="col">
+                      <div class="float-right">
+                        <button type="submit" class="btn btn-primary btn-md ">{{ __('SUBMIT') }}</button>
+                      </div>
+                    </div>
                   </div>
-                
+
+              </form>
+                  <hr>
+                  <p>Is this your first time here? Create account to register.</p>
+                  <a href="{{route('register')}}">
+                      <button type="submit" class="btn btn-primary btn-md btn-block ">{{ __('CREATE ACCOUNT') }}</button> 
+                  </a>
                 </div>
-                <div class="card-footer justify-content-center">
+                <div class="card-footer">
                  
-                    <button type="submit" class="btn btn-primary btn-md btn-block">{{ __('SUBMIT') }}</button>
-                    <br>
-                    <div>
-                    @if (Route::has('password.request'))
-                        <a href="{{ route('password.request') }}">
-                            <small>{{ __('Forgot password?') }}</small>
-                        </a>
-                    @endif
-                  </div>
                 </div>                 
                   
               </div>
-            </form>
+           
           
             <!-- <div class="copy-text">Created with <i class="fa fa-heart"></i> by Grafreez</div> -->
           </div>
