77dfd9d9ff70dad437578a65e058e26fa4fc725a
JulzPerez <julietoperez@gmail.com>
2021-10-25 12:21:46 -0700

    oct25-change4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,8 @@
     <meta http-equiv="refresh" content="{{ config('session.lifetime') * 60 }}">
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
     <!--     Fonts and icons     -->
-    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+    <link rel="stylesheet"  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200&display=swap" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
     <!-- CSS Files -->
     <link href="{{ asset('material') }}/css/material-dashboard.css?v=2.1.1" rel="stylesheet" />
--- resources/views/request/index.blade.php
+++ resources/views/request/index.blade.php
@@ -50,119 +50,58 @@
                         <p class="text-danger">You have no request created so far.</p>    
                         @else
                         <div class="table-responsive">
-                            <table class="table table-bordered ">
+                            <table class="table">
                                 <thead>
                                     <tr>
-                                        <th><strong>REQUEST</strong></th>
-                                        <th><strong>STATUS</strong></th>
+                                        <th><strong>REQUESTS</strong></th>
                                        
-                                        <th class="text-center"><strong>Actions</strong></th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                 @foreach($all_request as $key =>$request)
                                     <tr style="height:200px">
-                                        <td width="35%">
+                                        <td>
                                             <div class="form-row">
-                                                <div class="col" >
+                                                <div class="col-md-2" >
                                                     <label>Document </label>
                                                 </div>
-                                                <div class="col">
+                                                <div class="col-md-2">
                                                     <b>{{$request->docName.' '.$request->docParticular}}</b>   
                                                 </div>                                     
                                             </div>
 
                                             <div class="form-row">
-                                                <div class="col" >
+                                                <div class="col-md-2" >
                                                     <label>Request Date: </label>
                                                 </div>
-                                                <div class="col" >
+                                                <div class="col-md-2" >
                                                     <b>{{\Carbon\Carbon::parse($request->request_date)->toDateTimeString()}}</b>   
                                                 
                                                 </div>                                     
                                             </div>
-                                            
-                                        </td>
-                                        <td width="35%">
+
                                             <div class="form-row">
-                                                <div class="col" >
+                                                <div class="col-md-2" >
                                                     <label>Request Status </label>
                                                 </div>
-                                                <div class="col">
+                                                <div class="col-md-2">
                                                     <b>{{$request->request_status}}</b>   
                                                 </div>                                     
                                             </div>
+
                                             @if($request->auto_assess === 1 OR $request->request_status === 'assessed' 
                                                 OR $request->request_status === 'paid' OR $request->request_status === 'verified' 
                                                 OR $request->request_status === 'completed'
-                                                OR $request->request_status === 'processing')
+                                                OR $request->request_status === 'processing'
+                                                OR $request->request_status === 'released')
                                                 <div class="form-row">                                        
-                                                        <div class="col" >
+                                                        <div class="col-md-2" >
                                                             <label>Assessment Fee:   </label>                                                         
                                                         </div>
-                                                        <div class="col" >                                                           
+                                                        <div class="col-md-2" >                                                           
                                                             <b style="color:red">Php {{$request->assessment_total}}</b>
-                                                        </div>                                                                                                        
-                                                </div>
-                                                <div class="form-row">
-                                                    <div class="col" >
-                                                        <label>Assessed By:  </label>                                                         
-                                                    </div>
-                                                    <div class="col" >
-                                                     <b>{{$request->assessed_by}}  </b>                                                      
-                                                    </div>
-                                                </div>
-                                            @else
-                                                <div class="form-row">                                        
-                                                    <div class="col" >
-                                                        <label>Assessment Fee</label>
+                                                        </div> 
                                                         
-                                                    </div>   
-                                                    <div class="col" >
-                                                        <p>pending for assessment<p>                                                          
-                                                    </div>                                             
-                                                </div>
-                                                <div class="form-row">
-                                                    <div class="col" >
-                                                        <label>Assessed By:</label>                                                            
-                                                    </div>
-                                                    @if($request->assessed_by != 'auto assess')
-                                                    <div class="col" >
-                                                        <b>{{$request->assessed_by}}  </b>                                                        
-                                                    </div>
-                                                    @endif
-                                                </div>
-                                            @endif
-                                        </td>
-
-                                        <td class="td-actions text-right">
-                                            <div class="form-row">
-                                                <div class="col">
-                                                    @if($request->thread_id != null)
-                                                        <?php $count = Auth::user()->newThreadsCount(); ?>
-                                                    <a href="{{ route('messages.show', $request->thread_id) }}" >
-                                                        <button type="button" class="btn btn-secondary mt-1 mb-1" >
-                                                            <i class="material-icons">chat</i><b style="color:purple">View Message</b>
-                                                                @if($count > 0)
-                                                                    <span class="text-danger">New</span>
-                                                                @endif
-                                                        </button>
-                                                    </a>
-                                                    
-                                                    <!-- <a href="{{ route('messages.create', [$request->request_id, $request->requestor_id] ) }}" >
-                                                        <button type="button" class="btn btn-info" data-toggle="modal" data-target="#exampleModal">
-                                                            Create Message
-                                                        </button>
-                                                    </a> -->                                                  
-                                                    
-                                                    @endif
-                                                   
-                                                </div>  
-                                                                                      
-                                            </div>
-                                            
-                                            <div class="form-row">
-                                                <div class="col">
                                                         @if($request->request_status === 'assessed')
                                                                                                 
                                                         <div>
@@ -172,7 +111,7 @@
                                                                     $request->docName.' '.$request->docParticular,                                                                    
                                                                     ] 
                                                                 ) }}" >
-                                                                <button type="button" class="btn btn-secondary"><i class="material-icons">file_upload</i>
+                                                                <button type="button" class="btn btn-link btn-sm"><i class="material-icons">file_upload</i>
                                                                  <b style="color:blue">Upload Proof of Payment</b>
                                                                 </button>
                                                             <!-- <i class="fas fa-edit"></i> -->
@@ -180,11 +119,33 @@
                                                         </div>
                                                         <br>
                                                   
-                                                    @endif
+                                                    @endif                                                                                                       
                                                 </div>
-                                                                                    
-                                            </div>
+                                                <div class="form-row">
+                                                    <div class="col-md-2" >
+                                                        <label>Assessed By:  </label>                                                         
+                                                    </div>
+                                                    <div class="col-md-2" >
+                                                     <b>{{$request->assessed_by}}  </b>                                                      
+                                                    </div>
+                                                </div>
+                                            @else
+                                                <div class="form-row">                                        
+                                                    <div class="col-md-2" >
+                                                        <label>Assessment Fee</label>
+                                                        
+                                                    </div>   
+                                                    <div class="col-md-2" >
+                                                        <p>pending for assessment<p>                                                          
+                                                    </div>                                             
+                                                </div>
+                                               
+                                            @endif
+                                            
                                         </td>
+                                       
+
+                                       
                                     </tr>
                                 @endforeach
                                 </tbody>
